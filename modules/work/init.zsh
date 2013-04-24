958db72b16b72f5a9ff9b28baa5b5296ab9ca9ca
Mitch Garvin <garvinml@gmail.com>
2013-04-23 22:31:02 -0400

    work

--- modules/work/init.zsh
+++ modules/work/init.zsh
@@ -1,3 +1,5 @@
+alias ssh='TERM=rxvt ssh'
+
 function bypass(){
   sudo route del default
   sudo route add default gw 10.10.20.1
@@ -29,13 +31,23 @@ function mobi_pg_restore(){
   echo "Dropping then recreating $db..."
   dropdb $db
   createdb $db
+  echo "Creating List Files ..."
+  sans_billing='.sans_billing'
+  billing_shims='.billing_shims'
+  /usr/bin/rm $sans_billing
+  /usr/bin/rm $billing_shims
+  pg_restore -l $file | grep -iPv "(bill_usages|call_details|inventory_snapshots|inventory_items|discrepancies|resolved_by_id|3397;|3306;|3302;)" > $sans_billing
+  pg_restore -l $file | grep -iP "(bill_usages|call_details|inventory_snapshots|inventory_items|discrepancies|resolved_by_id|3397;|3306;|3302;)"  > $billing_shims
   echo "Restoring database $db..."
-  pg_restore -j 3 -O -d $db $file
+  #pg_restore -j 4 -O -x -d $db $file
+  pg_restore -j 4 -O -x -L $sans_billing -d $db $file
+  echo "Inserting Billing Shims"
+  pg_restore -j 4 -O -x -s -L $billing_shims -d $db $file
   echo "Importation of $db complete"
-  echo "Restoring database mobi_test (scheme only + data in schema_migrations)"
-  pg_restore -s -O -d mobi_test $file
-  pg_restore -t schema_migrations -O -d mobi_test $file
-  echo "Importation of mobi_test complete"
+  #echo "Restoring database mobi_test (scheme only + data in schema_migrations)"
+  #pg_restore -s -O -d mobi_test $file
+  #pg_restore -t schema_migrations -O -d mobi_test $file
+  #echo "Importation of mobi_test complete"
 }
 
 function mobi_pg_load(){
